060ce2dd37f8844a1214a58a093f14fbe17584c4
Chuyuan(Cyril) Liu <linkliucy@foxmail.com>
2022-07-31 22:47:46 +0800

    fix: pass Practice08Test

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,35 @@
 package practice08;
 
 public class Klass {
+    private int number;
+
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
