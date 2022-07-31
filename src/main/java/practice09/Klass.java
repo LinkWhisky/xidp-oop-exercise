c691e33ea112cda68c09602efdf73b2097a9c8f4
Chuyuan(Cyril) Liu <linkliucy@foxmail.com>
2022-07-31 23:34:35 +0800

    fix: pass Practice09Test

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,44 @@
 package practice09;
 
 public class Klass {
+    private int number;
+
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() == this) {
+            this.leader = student;
+        }else {
+            System.out.println("It is not one of us.");
+        }
+
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,37 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach Class "+ klass.getNumber() +".";
+    }
+
+    public String introduceWith(Student jerry) {
+        if (jerry.getKlass().getNumber() == this.getKlass().getNumber()) {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach "+ jerry.getName() +".";
+        }
+        return "My name is Tom. I am 21 years old. I am a Teacher. I don't teach " + jerry.getName() + ".";
+    }
 }
