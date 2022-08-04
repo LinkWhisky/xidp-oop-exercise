8efefbf780dfb1be73f09ce9ab58925cd9fc76db
Chuyuan(Cyril) Liu <linkliucy@foxmail.com>
2022-08-04 22:14:14 +0800

    fix: pass Practice11Test

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,47 @@
 package practice11;
 
+
 public class Klass {
+    private final int number;
+
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void setLeader(Student student) {
+        this.leader = student;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() == this) {
+            this.leader = student;
+            System.out.println("I am Tom. I know Jerry become Leader of Class 2.");
+        }else {
+            System.out.println("It is not one of us.");
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        System.out.println("I am Tom. I know Jerry has joined Class 2.");
+    }
+
+    public boolean isIn (Student student) {
+        return this.equals(student.getKlass());
+    }
+
 }
